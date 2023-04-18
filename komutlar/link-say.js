4c932530639961b6a98b8c80d6f41ab17e6e3d0b
MrBaskan <mrbaskan33@gmail.com>
2023-04-18 19:02:55 +0300

    Update link-say.js

--- komutlar/link-say.js
+++ komutlar/link-say.js
@@ -16,13 +16,15 @@ module.exports = {
       const Uptime = db.fetch(`UptimeLink_${interaction.user.id}`)
       const LinkLimit = db.fetch(`LinkLimit_${interaction.user.id}`) || 0
       let Limit = LinkLimit+3
-      
+      const Linkler = db.fetch(`UptimeLink`) || []
+      const KişiLinkleri = db.fetch(`UptimeLink_${interaction.user.id}`) || []
+
       if(!Uptime) {
         
       const SayYok = new EmbedBuilder()
          .setColor("Blurple")
          .setTitle("HarenUptime • Proje sayıları")
-         .addFields({name: `<:Belge:1046825193131225169> **Sistemdeki toplam projeler**`, value: `${db.fetch(`UptimeLink`) || [].length}`})
+         .addFields({name: `<:Belge:1046825193131225169> **Sistemdeki toplam projeler**`, value: `Linkler.length}`})
          .addFields({name: `<:Link:1046776084965900308> **Senin toplam projelerin**`, value: `Hiç link eklememişsin.`})
          .addFields({name: `<:Premium:1047169286659129487> **Toplam premium üyeler**`, value: `${db.fetch(`PremiumSayı`) || 0}`})
          .addFields({name: `<:Limit:1065321210847707227> **Link ekleme hakkın**`, value: `${Limit}`})
@@ -34,8 +36,8 @@ module.exports = {
       const Say = new EmbedBuilder()
          .setColor("Blurple")
          .setTitle("HarenUptime • Proje sayıları")
-         .addFields({name: `<:Belge:1046825193131225169> **Sistemdeki toplam projeler**`, value: `${db.fetch(`UptimeLink`) || [].length}`})
-         .addFields({name: `<:Link:1046776084965900308> **Senin toplam projelerin**`, value: `${db.fetch(`UptimeLink_${interaction.user.id}`) || [].length}`})
+         .addFields({name: `<:Belge:1046825193131225169> **Sistemdeki toplam projeler**`, value: `Linkler.length}`})
+         .addFields({name: `<:Link:1046776084965900308> **Senin toplam projelerin**`, value: `KişiLinkleri.length}`})
          .addFields({name: `<:Premium:1047169286659129487> **Toplam premium üyeler**`, value: `${db.fetch(`PremiumSayı`) || 0}`})
          .addFields({name: `<:Limit:1065321210847707227> **Link ekleme hakkın**`, value: `${Limit}`})
         
